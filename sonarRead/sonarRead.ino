10e68d4c4b7bf91ae887cf9cba6370d39545ae84
Fluvio <fluvio.lobo@gmail.com>
2016-03-06 15:47:32 -0500

    Modified the sonarRead program to read the voltage from a fixed analog pin of the arduino board. The reading is then printed on the serial monitor window.

--- sonarRead/sonarRead.ino
+++ sonarRead/sonarRead.ino
@@ -1,15 +1,21 @@
-/*
- *
- *
+/* Sonar Read
+ * Function designed to read the analog output of the LV-MaxSonar sensors by MaxBotix Inc.
+ * These sensors are used in the "Spider" prototype.
+ * Fluvio L. Lobo Fenoglietto 03/06/2016
  */
- 
- 
+
+int analogPin = 0;
+int val;
+
 void setup() {
   
+  Serial.begin(9600);
   
 } // End of setup loop
 
 void loop () {
   
+  val = analogRead(analogPin);
+  Serial.println(val);
   
 } // End of void loop
